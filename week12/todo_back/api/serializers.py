9cf9790de62299b09d40114359b1da6433bda714
Markitanov Denis <dmarkitanov@gmail.com>
2019-04-22 19:43:37 +0600

    tried with serializer

--- week12/todo_back/api/serializers.py
+++ week12/todo_back/api/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
 from .models import TaskList, Task
 
-class TaskListSerializer(serializers.ModelSerializer):
+class TaskListSerializer(serializers.Serializer):
+
+    name = serializers.CharField(required=True, allow_blank=False, max_length=200)
+
     class Meta:
         model = TaskList
         fields = '__all__'
 
     def create(self, validated_data):
         return TaskList.objects.create(**validated_data)
+
+    # def destroy(self, request, *args, **kwargs):
+    #    user = request.id # deleting user
+    #    # you custom logic # 
+    #    return super(YourViewSetClass, self).destroy(request, *args, **kwargs)
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
